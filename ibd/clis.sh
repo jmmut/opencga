e89e3c19b3999537c22af5f9ff67bee40dc4944a
jose miguel mut <jomutlo@gmail.com>
2015-11-04 12:13:02 +0000

    improving test case

--- ibd/clis.sh
+++ ibd/clis.sh
@@ -2,7 +2,7 @@ plink 1.9:
 /home/jmmut/usr/plink-1.9/plink --genome full --vcf ~/appl/opencga/ibd/test.vcf
 
 plink 1.07:
-/home/jmmut/usr/plink-1.07-x86_64/plink --genome --file ~/appl/opencga/ibd/test --noweb
+/home/jmmut/usr/plink-1.07-x86_64/plink --genome --file ~/appl/opencga/ibd/test --noweb --genome-full
 
 
 
